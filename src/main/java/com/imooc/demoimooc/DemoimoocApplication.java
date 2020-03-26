3621723f0fccb046d787ef4b951e3c96232c7f8f
gsm-p <280755874@qq.com>
2020-03-26 22:01:46 +0800

    first

---
+++ src/main/java/com/imooc/demoimooc/DemoimoocApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.demoimooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoimoocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoimoocApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/imooc/demoimooc/hello.java
@@ -0,0 +1,15 @@
+package com.imooc.demoimooc;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    public String hello()
+    {
+        return "Hello SpringBoot!";
+    }
+}
